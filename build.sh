c29b80285abb32957e02b726e96bacbaf47b2f22
Jonathan Springer <jonpspri@gmail.com>
2018-12-14 14:57:23 -0500

    Fix image name in build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ zlib_version=${ADOPTOPENJDK_ZLIB_VERSION:-1.2.11}
 target_registry=${ADOPTOPENJDK_TARGET_REGISTRY:-adoptopenjdk}
 
 docker build --build-arg ZLIB_VERSION="$zlib_version" \
-  -t "$target_registry/openjdk_glib_builder:${zlib_version}-$(uname -m)" \
+  -t "$target_registry/openjdk-zlib-builder:${zlib_version}-$(uname -m)" \
   ./builder-image/.
 
-docker push "$target_registry/openjdk_glib_builder:${zlib_version}-$(uname -m)"
+docker push "$target_registry/openjdk-zlib-builder:${zlib_version}-$(uname -m)"
